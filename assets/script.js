0e66799e5b8dc3e4e49c914b18ef38062d2ac08a
Luke Thompson <16175583curtin@gmail.com>
2021-06-18 16:12:37 +0800

    Baseline working. Abandoned old method of random character selections for basic strings and for loop

--- assets/script.js
+++ assets/script.js
@@ -1,44 +1,172 @@
-// Assignment Code
+//Character sets
+
+var lower = 'abcdefghijklmnopqrstuvwxyz';
+var upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var numbersvar = '0123456789';
+var symbol = '!@#$%^&*=-_';
+var pass;
+
+// Points to generate password button on page
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  
+  var password1 = generatePassword();
   var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+  
+  passwordText.value = password1;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-//--------
 
 
+//Set length and toggle character types (user input)
+function generatePassword() {
+
+  var passLength = window.prompt('How many characters long will your password be?');
+
+  if (!passLength) {
+		return;
+	}
+
+  if(isNaN(passLength)) {
+    window.alert('You can only input numbers.')
+    return;
+  }
+  if (passLength < 8) {
+		window.alert('You must create a password that is at least 8 characters long.');
+		return;
+	}
+    isLower = window.confirm('Do you want to use lower case characters in your password?');
+  
+    isUpper = window.confirm('Do you want to use upper case characters in your password?');
+  
+    isNumber = window.confirm('Do you want to use numbers in your password?')
+  
+    isSymbol = window.confirm('Do you want to use symbols in your password?');
+  
+  if (!isLower && !isUpper && !isNumber && !isSymbol) {
+     window.alert('You must select at least one character type for your password.')
+    return;
+   }
+  console.log(passLength, isLower, isUpper, isNumber, isSymbol);
+
+//--------------------- 1
+
+  if (isLower && isUpper && isNumber && isSymbol) {
+    userChoices = lower.concat(upper, numbersvar, symbol);
+    console.log(userChoices);
+ 
+  }
+
+  if (isLower && isUpper && isNumber && !isSymbol) {
+    userChoices = lower.concat(upper, numbersvar);
+    console.log(userChoices);
+ 
+  }
+
+  if (isLower && isUpper && !isNumber && !isSymbol) {
+    userChoices = lower.concat(upper);
+    console.log(userChoices);
+ 
+  }
+
+  if (isLower && !isUpper && !isNumber && !isSymbol) {
+    userChoices = lower;
+    console.log(userChoices);
+ 
+  }
+
+  //--------------------- 2
+
+  if (isUpper && isLower && isNumber && isSymbol) {
+    userChoices = upper.concat(lower, numbersvar, symbol);
+    console.log(userChoices);
+ 
+  }
+
+  if (isUpper && isLower && isNumber && !isSymbol) {
+    userChoices = upper.concat(lower, numbersvar);
+    console.log(userChoices);
+ 
+  }
+
+  if (isUpper && isLower && !isNumber && !isSymbol) {
+    userChoices = upper.concat(lower);
+    console.log(userChoices);
+ 
+  }
+
+  if (isUpper && !isLower && !isNumber && !isSymbol) {
+    userChoices = upper;
+    console.log(userChoices);
+ 
+  }
 
 
-const randomFunc = {
-  lower: getRandomLower,
-  upper: getRandomUpper,
-  number: getRandomNumber,
-  symbol: getRandomSymbol,
-};
+  //--------------------- 3
 
-// Generate Characters
-// Generate lower case
-function getRandomLower() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
-// Generate upper case
-function getRandomUpper() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
-// Generate number
-function getRandomNumber() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
-// Generate symbol
-function getRandomSymbol() {
-  const symbols = '!#$%&()*+,-./:;<=>?@[\]^_`{|}~';
-  return symbols[Math.floor(Math.random() * symbols.length)];
+  if (isLower && isUpper && isNumber && isSymbol) {
+    userChoices = lower.concat(upper, numbersvar, symbol);
+    console.log(userChoices);
+ 
+  }
+
+  if (isLower && isUpper && isNumber && !isSymbol) {
+    userChoices = lower.concat(upper, numbersvar);
+    console.log(userChoices);
+ 
+  }
+
+  if (isLower && isUpper && !isNumber && !isSymbol) {
+    userChoices = lower.concat(upper);
+    console.log(userChoices);
+ 
+  }
+
+  if (isLower && !isUpper && !isNumber && !isSymbol) {
+    userChoices = lower;
+    console.log(userChoices);
+ 
+  }
+
+  //--------------------- 4
+
+  if (isLower && isUpper && isNumber && isSymbol) {
+    userChoices = lower.concat(upper, numbersvar, symbol);
+    console.log(userChoices);
+ 
+  }
+
+  if (isLower && isUpper && isNumber && !isSymbol) {
+    userChoices = lower.concat(upper, numbersvar);
+    console.log(userChoices);
+ 
+  }
+
+  if (isLower && isUpper && !isNumber && !isSymbol) {
+    userChoices = lower.concat(upper);
+    console.log(userChoices);
+ 
+  }
+
+  if (isLower && !isUpper && !isNumber && !isSymbol) {
+    userChoices = lower;
+    console.log(userChoices);
+ 
+  }
+
+  // ---------
+  var passwordBlank =[];
+
+  for (var i = 0; i < passLength; i++) {
+    var passwordstring = userChoices[Math.floor(Math.random() * userChoices.length)];
+    passwordBlank.push(passwordstring);
+  }
+
+  var password1 = passwordBlank.join("");
+  return password1;
 }
