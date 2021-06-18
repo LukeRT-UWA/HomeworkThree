a19fa788a9ba1a30536eb77fe9b97f835b4dcc53
Luke Thompson <16175583curtin@gmail.com>
2021-06-18 12:33:40 +0800

    created functions to generate upper, lower, number and special characters

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # HomeworkThree
 
 Assignment number three.
+
+https://www.net-comber.com/charset.html
+
+https://www.owasp.org/index.php/Password_special_characters
--- assets/script.js
+++ assets/script.js
@@ -12,3 +12,33 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+//--------
+
+
+
+
+const randomFunc = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol,
+};
+
+// Generate Characters
+// Generate lower case
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+// Generate upper case
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+// Generate number
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+// Generate symbol
+function getRandomSymbol() {
+  const symbols = '!#$%&()*+,-./:;<=>?@[\]^_`{|}~';
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
